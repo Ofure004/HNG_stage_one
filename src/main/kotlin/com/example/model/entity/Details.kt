af3d1168704d6197e6b52b40c0efdc1b77a8ffdf
Ofure004 <ofure.ehiremhen@stu.cu.edu.ng>
2023-09-13 16:04:01 +0100

    Second commit

--- src/main/kotlin/com/example/model/entity/Details.kt
+++ src/main/kotlin/com/example/model/entity/Details.kt
@@ -2,15 +2,16 @@ package com.example.model.entity
 
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
-import java.util.*
+import java.time.*
+import java.time.format.DateTimeFormatter
 
 @Serializable
 data class Details(
-    var slackName : String? = null,
-    var currentDay: String = Calendar.getInstance().get(Calendar.DAY_OF_WEEK).toString(),
-    var utcTime : String = LocalDateTime.now().toString(),
-    var track : String? = null,
-    var githubFileUrl : String = "https://bleh",
-    var githubRepoUrl : String = "https://github.com/Ofure004/HNG_stage_one",
+    var slackName: String? = null,
+    var currentDay: DayOfWeek = LocalDate.now().getDayOfWeek(),
+    var utcTime: String = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")),
+    var track: String? = null,
+    var githubFileUrl: String = "https://github.com/Ofure004/HNG_stage_one/blob/master/src/main/kotlin/com/example/Application.kt",
+    var githubRepoUrl: String = "https://github.com/Ofure004/HNG_stage_one",
     var status_code: Int? = 200
 )
